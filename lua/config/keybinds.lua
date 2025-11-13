894e6e9fb5cb1cfc01a77a55b181c86088426c39
SirGoatsalot <fostersullivan12@gmail.com>
2025-11-13 13:23:56 -1000

    Modify netrw keybind

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,7 +1,6 @@
 -- Set Keybinds for Neovim
 
--- Browse net dir
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Browse netrw directory" })
 
 -- Telescope bindings
 local builtin = require("telescope.builtin")
@@ -10,7 +9,7 @@ vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = 'Telescope git fil
 vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = 'Telescope string' })
 vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = 'Telescope treesitter' })
 
--- Controls for termninal mode
+-- Controls for termninal mode when using split windows
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = 'Exit terminal mode' })
 vim.keymap.set("t", "<A-esc>", "<C-\\><C-n><C-w>h", { desc = 'Exit terminal mode, move back to left buffer' })
 vim.keymap.set("n", "<A-esc>", "<C-w>li", { desc = 'Enter terminal to right' })
