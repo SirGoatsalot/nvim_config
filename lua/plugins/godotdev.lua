ed883e8d4c77e2a8296c450acd2d89783bb3614c
Foster Sullivan <fostersullivan12@gmail.com>
2026-04-14 10:31:09 -1000

    Replace vimgodot plugin with godotdev plugin.

---
+++ lua/plugins/godotdev.lua
@@ -0,0 +1,13 @@
+return {
+	{
+  		'Mathijs-Bakker/godotdev.nvim',
+  		dependencies = {
+			'nvim-lspconfig',
+			'mfussenegger/nvim-dap',
+			'nvim-neotest/nvim-nio',
+			'rcarriga/nvim-dap-ui',
+			'nvim-treesitter',
+			'habamax/vim-godot'
+		},
+	}
+}
